6f7dddc2bad02d20420ac6ae1010945f8a47b2dd
AhmedTarek215 <at037940@gmail.com>
2025-05-05 18:29:13 +0300

    Item_comment

--- app/src/main/java/com/example/petto/data/model/PetService.kt
+++ app/src/main/java/com/example/petto/data/model/PetService.kt
@@ -3,12 +3,14 @@ package com.example.petto.data.model
 data class PetService(
     val service_id: String = "",
     val name: String = "",
-    val service_img: String = "",
+    val imageUrl: String = "",
     val service_type: String = "",
     val social_media: String? = null,
     val services: List<String> = listOf(),
     val contact_info: String = "",
-    val location: String = ""
+    val location: String = "",
+    val documentId: String = "" ,// we'll set this manually when reading from Firestore
+    val average_rating: Double = 0.0
 )
 
 
