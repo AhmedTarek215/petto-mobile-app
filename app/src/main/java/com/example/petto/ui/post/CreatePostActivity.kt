d40460d32a99f9bdde7028566d627fe57e532282
AhmedTarek215 <at037940@gmail.com>
2025-05-16 17:07:42 +0300

    polishes

--- app/src/main/java/com/example/petto/ui/post/CreatePostActivity.kt
+++ app/src/main/java/com/example/petto/ui/post/CreatePostActivity.kt
@@ -1,15 +1,19 @@
 package com.example.petto.ui.post
 
+//import android.util.Log
+//import com.example.petto.HomeActivity
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-//import android.util.Log
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.petto.R
-//import com.example.petto.HomeActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
@@ -35,7 +39,6 @@ class CreatePostActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_post)
 
         editTextPost = findViewById(R.id.editTextPost)
-        btnAttach = findViewById(R.id.btnAttach)
         btnPost = findViewById(R.id.btnPost)
         btnCancel = findViewById(R.id.btnCancel)
         backButton = findViewById(R.id.backButton)
