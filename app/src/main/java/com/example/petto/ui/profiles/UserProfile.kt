a3ecd2cd80dc0c30e6e0f04c450b86e367a09aa1
jouirya <zjouirya@gmail.com>
2025-05-17 23:24:22 +0300

    profile pages

--- app/src/main/java/com/example/petto/ui/profiles/UserProfile.kt
+++ app/src/main/java/com/example/petto/ui/profiles/UserProfile.kt
@@ -9,6 +9,8 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.petto.R
+import com.example.petto.ui.notification.NotificationActivity
+import com.example.petto.ui.post.CreatePostActivity
 import com.example.petto.ui.post.MyPostsActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -63,7 +65,18 @@ class UserProfile : AppCompatActivity() {
         if (userId.isNotEmpty()) {
             loadUserData()
         }
+        navNotifications.setOnClickListener {
+            startActivity(Intent(this, NotificationActivity::class.java))
+            finish()
+        }
 
+        navProfile.setOnClickListener {
+            // You are already here, so no action needed
+        }
+
+        fabAdd.setOnClickListener {
+            startActivity(Intent(this, CreatePostActivity::class.java))
+        }
         btnMyPets.setOnClickListener {
             startActivity(Intent(this, PetProfile::class.java))
         }
